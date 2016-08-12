82844d7dc8c46bf003a30628daf6bdb842a9ab03
wangmingxu <wangmingxu@qipeipu.com>
2016-08-12 21:21:33 +0800

    add

---
+++ js/Flide.js
@@ -0,0 +1,216 @@
+/**
+ * Created by wmx on 2016/8/12.
+ */
+/**
+  默认html模板
+  <div class="slide_list">
+    <div class="item" data-role="item">
+      <img1>
+    </div>
+    <div class="item" data-role="item">
+      <img2>
+    </div>
+    ...
+  </div>
+ */
+/*
+默认CSS
+.slide_list{
+  overflow: hidden;
+  display: flex;
+  justify-content:space-between;
+  align-items:center;
+  .item{
+    flex-shrink:0;
+    margin-right: ??;
+    width: ??;
+    height: ??;
+    img{
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+ */
+(function(global, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // Register as an anonymous AMD module:
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS:
+        factory(require('jquery'));
+    } else {
+        // Browser globals:
+        factory(window.jQuery);
+    }
+})(this, function($) {
+
+    var NAME = "Flide",
+        VERSION = "1.0.0",
+        FLIDE_KEY = 'flide',
+        DATA_KEY = 'mx-flide',
+        JQUERY_NO_CONFLICT = $.fn[NAME];
+
+    var Default = {
+        initfocus: 0, //默认焦点第一张
+        sPicWidth: 310, //非焦点图宽度
+        sPicHeight: 218, //非焦点图宽度
+        bPicWidth: 418, //焦点图宽度
+        bPicHeight: 295, //焦点图高度
+        picMargin: 20, //图片间距
+        showPicCount: 3, //容器内最多显示3张
+        perSlide: 1, //每次滑动1张
+        autoSlide: false, //是否自动滑动
+        speed: 200 //滑动速度
+    };
+
+    var Flide = (function($) {
+        var Flide = function(element, option) {
+            this._option = this._getOption(option);
+            this._$element = $(element);
+            this._$element.data(FLIDE_KEY, this);
+            this.nowFocus = this._option.initfocus;
+            this.leftMax = 0;
+            this.rightMax = this._$element.find("[data-role='item']").length - 1;
+            this.mLeft = (this.nowFocus - 2) < this.leftMax ? this.leftMax : this.nowFocus - 2;
+            this.mRight = (this.nowFocus + 2) > this.rightMax ? this.rightMax : this.nowFocus + 2;
+            this._init(this.nowFocus);
+        };
+
+        Flide.prototype._init = function() {
+            // this._calcWidth();
+            this.tabTo(this.nowFocus);
+            this._initEventHandle();
+        };
+
+
+        Flide.prototype._initEventHandle = function() {
+            $(document).on('click', "[data-role='next']", $.proxy(this.next, this));
+            $(document).on('click', "[data-role='prev']", $.proxy(this.prev, this));
+        };
+
+        Flide.prototype._offEventHandle = function() {
+            $(document).off('click', "[data-role='next']");
+            $(document).off('click', "[data-role='prev']");
+        };
+
+        Flide.prototype._calcWidth = function() {
+            var wrapperWidth,
+                picWidth,
+                marginWidth;
+
+            picWidth = this._option.bPicWidth + this._option.sPicWidth * (showPicCount - 1);
+            marginWidth = this.picMargin * (showPicCount - 1);
+            wrapperWidth = picWidth + marginWidth;
+            this._$element.css('width', wrapperWidth + 'px');
+        };
+        Flide.prototype._calcOffset = function() {
+            if (this.nowFocus < this._option.showPicCount) {
+                return 0;
+            }
+            var leftOffset = -(this.nowFocus - this._option.showPicCount + 1) * this._option.sPicWidth;
+            return leftOffset;
+        };
+        Flide.prototype.next = function() {
+            this.nowFocus++;
+            this.judgeMax();
+            this.judgeSlide("next");
+            var speed = this._option.speed;
+            this._$element.find("[data-role='item']").eq(this.nowFocus - 1).animate({
+                width: this._option.sPicWidth + "px",
+                height: this._option.sPicHeight + "px"
+            }, speed);
+            this._$element.find("[data-role='item']").eq(this.nowFocus).animate({
+                width: this._option.bPicWidth + "px",
+                height: this._option.bPicHeight + "px"
+            }, speed);
+        };
+        Flide.prototype.prev = function() {
+            this.nowFocus--;
+            this.judgeMax();
+            this.judgeSlide("prev");
+            var speed = this._option.speed;
+            this._$element.find("[data-role='item']").eq(this.nowFocus + 1).animate({
+                width: this._option.sPicWidth + "px",
+                height: this._option.sPicHeight + "px"
+            }, speed);
+            this._$element.find("[data-role='item']").eq(this.nowFocus).animate({
+                width: this._option.bPicWidth + "px",
+                height: this._option.bPicHeight + "px"
+            }, speed);
+        };
+        Flide.prototype.tabTo = function(i) {
+
+        };
+        Flide.prototype.judgeSlide = function(type) {
+            var Offset = this._calcOffset() + "px";
+            var nowFocus = this.nowFocus;
+            if (nowFocus >= this.mLeft && nowFocus <= this.mRight) {
+                $("[data-role='next']").show();
+                $("[data-role='prev']").show();
+                return;
+            }
+            switch (type) {
+                case "prev":
+                    this.mLeft--;
+                    this.mRight--;
+                    break;
+                case "next":
+                    this.mLeft++;
+                    this.mRight++;
+                    break;
+            }
+            var speed = this._option.speed;
+            this._$element.find("[data-role='item']").eq(0).animate({
+                marginLeft: Offset
+            }, speed);
+        };
+        Flide.prototype.judgeMax = function() {
+            var leftMax = 0;
+            var rightMax = this._$element.find("[data-role='item']").length - 1;
+            var speed = this._option.speed;
+            if (this.nowFocus == this.leftMax) {
+                $("[data-role='prev']").fadeOut(speed);
+            } else if (this.nowFocus == this.rightMax) {
+                $("[data-role='next']").fadeOut(speed);
+            }
+        };
+        Flide.prototype._getOption = function(option) {
+            return $.extend(true, {}, Default, option);
+        };
+        Flide.prototype.destroy = function() {
+            this._$element.removeData(FLIDE_KEY);
+            this._offEventHandle();
+            delete this;
+        };
+        Flide._jQueryInterface = function(config) {
+            var args = [].slice.call(arguments, 1);
+            return this.each(function(i, elem) {
+
+                var data,
+                    _config,
+                    flide;
+
+                data = $(elem).data(DATA_KEY);
+                _config = $.extend(true, {}, data, typeof config === 'object' ? config : null);
+                flide = $(elem).data(FLIDE_KEY);
+                if (!flide) {
+                    flide = new Flide(elem, _config);
+                }
+                if (typeof config == "string") {
+                    flide[config](args);
+                }
+            });
+        };
+        return Flide;
+    })($);
+
+    $.fn[NAME] = Flide._jQueryInterface;
+    $.fn[NAME].Constructor = Flide;
+    $.fn[NAME].noConflict = function() {
+        $.fn[NAME] = JQUERY_NO_CONFLICT;
+        return Flide._jQueryInterface;
+    };
+
+    typeof module == 'object' && (module.exports = Flide);
+});
